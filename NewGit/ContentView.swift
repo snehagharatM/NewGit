ab8cdcf5175771ddfe9e0bbfda03be49854bec85
snehagharatM <sneha.mobilefirst@gmail.com>
2024-02-21 12:36:14 +0530

    Initial Commit

---
+++ NewGit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NewGit
+//
+//  Created by sneha gharat on 21/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NewGit/NewGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewGitApp.swift
+//  NewGit
+//
+//  Created by sneha gharat on 21/02/24.
+//
+
+import SwiftUI
+
+@main
+struct NewGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
