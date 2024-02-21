e2c94be1b7b2ba1e693f5fdfaaf356a14e1109de
snehagharatM <sneha.mobilefirst@gmail.com>
2024-02-21 13:46:04 +0530

    green

--- NewGit/ContentView.swift
+++ NewGit/ContentView.swift
@@ -15,7 +15,7 @@ struct ContentView: View {
             Text("hhhsb is this you...?")
             Text("wwess is this you...?")
         }
-        .foregroundColor(.red)
+        .foregroundColor(.green)
         .navigationTitle("Hello")
         .padding()
     }
